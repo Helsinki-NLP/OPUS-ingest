d649f47102af20f2139826d6f7a66abc6288497c
Jörg Tiedemann <jorg.tiedemann@helsinki.fi>
2017-07-01 22:21:50 +0300

    installation scripts for letsmt and pdf2xml

--- modules/opus/0.1.lua
+++ modules/opus/0.1.lua
@@ -16,9 +16,23 @@ local version = "0.1"
 
 prepend_path("PATH", pathJoin(basepath, "bin"))
 prepend_path("PATH", pathJoin(basepath, "opus-tools/0.02/bin"))
+prepend_path("PATH", pathJoin(basepath, "pdf2xml/0.2.7/bin"))
+prepend_path("PATH", pathJoin(basepath, "letsmt/dev/bin"))
+
 prepend_path("PERL5LIB", pathJoin(basepath, "lib64/perl5"))
 prepend_path("PERL5LIB", pathJoin(basepath, "share/perl5"))
+
 prepend_path("PERL5LIB", pathJoin(basepath, "opus-tools/0.02/share/perl5"))
+prepend_path("PERL5LIB", pathJoin(basepath, "pdf2xml/0.2.7/share/perl5"))
+prepend_path("PERL5LIB", pathJoin(basepath, "letsmt/dev/share/perl5"))
+prepend_path("PERL5LIB", pathJoin(basepath, "letsmt/dev/lib64/perl5"))
+
+prepend_path("PYTHONPATH", pathJoin(basepath, "letsmt/dev/lib/python2.6/site-packages"))
+
 prepend_path("LD_LIBRARY_PATH", pathJoin(basepath, "lib"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(basepath, "letsmt/dev/lib"))
+
 prepend_path("MANPATH", pathJoin(basepath, "share/man"))
 prepend_path("MANPATH", pathJoin(basepath, "opus-tools/0.02/share/man"))
+prepend_path("MANPATH", pathJoin(basepath, "pdf2xml/0.2.7/share/man"))
+prepend_path("MANPATH", pathJoin(basepath, "letsmt/dev/share/man"))
