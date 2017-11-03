ffca9722d0e1d1e5d9960a0c897c06aa825b7dea
Jörg Tiedemann <jorg.tiedemann@helsinki.fi>
2017-11-03 21:57:37 +0200

    fixes

--- admin/opus2abel.sh
+++ admin/opus2abel.sh
@@ -6,7 +6,7 @@
 #SBATCH -J opus2abel
 #SBATCH -t 3-00:00:00
 #SBATCH -N 1
-#SBATCH -n 4
+#SBATCH -n 1
 #SBATCH --mem=4g
 #SBATCH --mail-type=END
 #SBATCH --mail-user=jorg.tiedemann@helsinki.fi
@@ -15,21 +15,36 @@
 OPUS_TAITO=/proj/nlpl/data/OPUS
 OPUS_ABEL=joerg@abel.uio.no:/projects/nlpl/data/OPUS
 
-CORPORA="Books DGT DOGC EMEA EUbookshop \
-    Europarl2 Europarl3-clean Europarl \
-    hrenWaC JRC-Acquis \
-    MBS MultiUN News-Commentary11 OfisPublik OpenOffice3 \
-    OpenSubtitles \
-    RF Tanzil TED2013 TedTalks TEP \
-    UN Wikipedia WikiSource WMT-News \
-    ECB EUconst GlobalVoices GNOME KDE4 KDEdoc \
-    Tatoeba PHP Ubuntu"
+cd $OPUS_TAITO
 
-cd $OPUS_TAITO/corpus
+## only sync raw xml files to abel
 
-## sync selected corpora
+rsync -ahv \
+    --include '*/' \
+    --include '**/raw/**/*.xml.gz' \
+    --exclude '*' \
+    corpus ${OPUS_ABEL}/
 
-echo -n $CORPORA | parallel -d ' ' rsync --delete -ahv {} ${OPUS_ABEL}/corpus/
+
+
+
+# CORPORA="Books DGT DOGC EMEA EUbookshop \
+#     Europarl2 Europarl3-clean Europarl \
+#     hrenWaC JRC-Acquis \
+#     MBS MultiUN News-Commentary11 OfisPublik OpenOffice3 \
+#     OpenSubtitles \
+#     RF Tanzil TED2013 TedTalks TEP \
+#     UN Wikipedia WikiSource WMT-News \
+#     ECB EUconst GlobalVoices GNOME KDE4 KDEdoc \
+#     Tatoeba PHP Ubuntu"
+
+# ## sync selected corpora
+
+# echo -n $CORPORA | parallel -d ' ' \
+# rsync --delete --delete-excluded \
+#     --exclude '*' \
+#     --include '*/raw/*.xml.gz' \
+#     -ahv {} ${OPUS_ABEL}/corpus/
 
 
 ## sync all data in corpus
