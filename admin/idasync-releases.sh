2aec929dd669bb4ee060d2c482d3e7bf96476311
Jörg Tiedemann <jorg.tiedemann@helsinki.fi>
2019-07-21 15:30:43 +0300

    better depenencies in wordalign Makefile

--- admin/idasync-releases.sh
+++ admin/idasync-releases.sh
@@ -23,16 +23,16 @@ TRGDIR=/ida/sa/clarin/corpora/OPUS/releases
 
 ## all sub-corpora (that don't have to be packaged)
 
-CORPORA="Books DGT DOGC \
+CORPORA="ada83 Bianet Books CAPES DGT DOGC \
          EhuHac Elhuyar \
-         EMEA EUbookshop Europarl \
+         EMEA EUbookshop Europarl Finlex fiskmo \
          giga-fren hrenWaC JRC-Acquis \
          MBS memat MontenegrinSubs MPC1 MultiUN \
          News-Commentary \
          OfisPublik OpenOffice \
          ParaCrawl \
-         RF SETIMES SPC Tanzil TED2013 TedTalks TEP \
-         UN Wikipedia WikiSource WMT-News XhosaNavy"
+         RF sardware SciELO SETIMES SPC Tanzil TED2013 TedTalks TEP \
+         UN UNPC wikimedia Wikipedia WikiSource WMT-News XhosaNavy"
 
 
 ## leave "OpenSubtitles" as a separate job
@@ -45,7 +45,9 @@ CORPORA="Books DGT DOGC \
 
 TARCORPORA="ECB EUconst GlobalVoices GNOME \
             KDE4 KDEdoc \
-            PHP Tatoeba Ubuntu"
+            PHP QED Tatoeba Ubuntu"
+
+## Debian infopankki
 
 for c in ${TARCORPORA}; do
     if [ -f "$SRCDIR/$c.tar.gz" ]; then
