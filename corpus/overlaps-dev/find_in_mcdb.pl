ca4183dbc092a220c3fb709c643aeda66cbb2cc6
Joerg Tiedemann <jorg.tiedemann@helsinki.fi>
2022-11-11 09:30:58 +0200

    ELRC data imports

---
+++ corpus/overlaps-dev/find_in_mcdb.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+#-*-perl-*-
+
+use utf8;
+use strict;
+use MCDB_File ();
+
+
+my $file = shift(@ARGV);
+my $count = 0;
+my $found = 0;
+
+binmode(STDIN);
+binmode(STDOUT);
+
+my %db = ();
+my $mcdb = MCDB_File->TIEHASH($file);
+while (<>){
+    $count++;
+    if ($mcdb->EXISTS($_)) {
+	$found++;
+    }
+    print STDERR '.' if (! ($count % 50000));
+    print STDERR " $count\n" if (! ($count % 2500000));
+}
+
+undef $mcdb;
+print $found;
